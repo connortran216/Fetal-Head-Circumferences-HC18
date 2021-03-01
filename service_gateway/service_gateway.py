ced4d8d48501feee75dde50d2d8c80499a6e8971
connortran216 <connortran123@gmail.com>
2021-03-01 13:58:09 +0700

    add swagger

--- service_gateway/service_gateway.py
+++ service_gateway/service_gateway.py
@@ -1,8 +1,11 @@
 import uvicorn
 from fastapi import FastAPI, Form, UploadFile, File
+from fastapi.openapi.utils import get_openapi
 import requests
 
-api_gateway = FastAPI(title='API Gateway')
+api_gateway = FastAPI(title='API Gateway',
+					  description='Fetal Head Circumferences Estimator.',
+					  version='1.0.0')
 
 global url
 
@@ -30,11 +33,26 @@ async def insert(pixel_size: str = Form(...), file: UploadFile = File(...)):
 	return res
 
 
+def custom_openapi():
+	if api_gateway.openapi_schema:
+		return api_gateway.openapi_schema
+	openapi_schema = get_openapi(
+		title="API Gateway",
+		version="1.0.0",
+		description="Fetal Head Circumferences Estimator",
+		routes=api_gateway.routes,
+	)
+	api_gateway.openapi_schema = openapi_schema
+	return api_gateway.openapi_schema
+
+
 if __name__ == "__main__":
 	#URL
 	url = [
 		"http://centerprocessing:8000/request_mask",
 	]
-
+	api_gateway.openapi = custom_openapi
 	# host = 'localhost' if run local else 'servicegateway'
-	uvicorn.run(api_gateway, port=8888, host='servicegateway', debug=True)
+	uvicorn.run(api_gateway, port=8888, host='localhost', debug=True)
+
+
